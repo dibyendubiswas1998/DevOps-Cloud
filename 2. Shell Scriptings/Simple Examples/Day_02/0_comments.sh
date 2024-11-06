3188c7172feda083c5295da31fa42ea03a54d6c5
Dibyendu Biswas <67157274+dibyendubiswas1998@users.noreply.github.com>
2024-11-06 09:05:01 +0530

    Create 0_comments.sh

---
+++ 2. Shell Scriptings/Simple Examples/Day_02/0_comments.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+echo "Single line comments: using '#'"
+# single line comments.
+
+
+echo "Multiline comments: using '<<comment ........ comment' "
+<<comment
+This is
+Multi line
+Comments.
+Thank You!!!!!!!!!!!
+comment
